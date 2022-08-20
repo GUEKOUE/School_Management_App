fa803aa2abde2e5111cccac4e0c8006a8f87e09b
GUEKOUE <compassiondemenou@yahoo.com>
2022-08-20 01:01:57 +0100

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('postgres://mtfphrbwgsgvmp:d17ace962f92c8a6d0b2c0d1f45075b4d6314cd0f3e40fb7a604b140a91ceeca@ec2-52-207-15-147.compute-1.amazonaws.com:5432/d6koppst7ruujt'),
+            'host' => env('DB_HOST', 'ec2-52-207-15-147.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd6koppst7ruujt'),
+            'username' => env('DB_USERNAME', 'mtfphrbwgsgvmp'),
+            'password' => env('DB_PASSWORD', 'd17ace962f92c8a6d0b2c0d1f45075b4d6314cd0f3e40fb7a604b140a91ceeca'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
